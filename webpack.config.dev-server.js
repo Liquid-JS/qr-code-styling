0302fabf92e124cf36d16a2ed5fdd92cbb3c754f
HitkoDev <development@hitko.si>
2022-05-05 20:05:23 +0200

    chore: update dependencies

--- webpack.config.dev-server.js
+++ webpack.config.dev-server.js
@@ -5,13 +5,14 @@ const commonConfig = require('./webpack.config.common.js');
 module.exports = merge(commonConfig, {
   mode: 'development',
   devServer: {
-    injectClient: false //workaround for bug https://github.com/webpack/webpack-dev-server/issues/2484
+    webSocketServer: false //workaround for bug https://github.com/webpack/webpack-dev-server/issues/2484
   },
   devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      inject: 'head'
+      inject: 'head',
+      scriptLoading: 'blocking'
     })
   ]
 });
