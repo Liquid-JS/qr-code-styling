0200cf387f05cd3a1b047f42ecb4d02c1e2ba6f4
HitkoDev <development@hitko.si>
2024-03-21 10:17:57 +0100

    chore: lint

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,34 +1,26 @@
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
-import typescript from 'rollup-plugin-typescript2';
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "rollup-plugin-typescript2";
 
 export default [
-    {
-        input: 'src/index.ts',
-        output: {
-            file: './lib/qr-code-styling.js',
-            sourcemap: true,
-            format: 'umd',
-            name: 'QRCodeStyling'
-        },
-        plugins: [
-            typescript(),
-            commonjs(),
-            nodeResolve(),
-            terser()
-        ]
+  {
+    input: "src/index.ts",
+    output: {
+      file: "./lib/qr-code-styling.js",
+      sourcemap: true,
+      format: "umd",
+      name: "QRCodeStyling"
     },
-    {
-        input: 'src/node.ts',
-        output: {
-            file: './lib/qr-code-styling-node.js',
-            sourcemap: true,
-            format: 'cjs'
-        },
-        plugins: [
-            typescript(),
-            commonjs()
-        ]
-    }
+    plugins: [typescript(), commonjs(), nodeResolve(), terser()]
+  },
+  {
+    input: "src/node.ts",
+    output: {
+      file: "./lib/qr-code-styling-node.js",
+      sourcemap: true,
+      format: "cjs"
+    },
+    plugins: [typescript(), commonjs()]
+  }
 ];
