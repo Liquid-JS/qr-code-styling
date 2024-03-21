097dae9219e1e34efa88b49d649837e02b0f8ec5
HitkoDev <development@hitko.si>
2024-03-21 10:15:29 +0100

    feat: use vite & rollup rather than webpack, fix exports

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,34 @@
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
-export default {
-    input: 'src/node.ts',
-    output: {
-        file: './lib/qr-code-styling-node.js',
-        sourcemap: true,
-        format: 'cjs'
+export default [
+    {
+        input: 'src/index.ts',
+        output: {
+            file: './lib/qr-code-styling.js',
+            sourcemap: true,
+            format: 'umd',
+            name: 'QRCodeStyling'
+        },
+        plugins: [
+            typescript(),
+            commonjs(),
+            nodeResolve(),
+            terser()
+        ]
     },
-    plugins: [typescript(), commonjs()]
-};
+    {
+        input: 'src/node.ts',
+        output: {
+            file: './lib/qr-code-styling-node.js',
+            sourcemap: true,
+            format: 'cjs'
+        },
+        plugins: [
+            typescript(),
+            commonjs()
+        ]
+    }
+];
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { ErrorCorrectionPercents } from "./constants/errorCorrectionPercents";
 import { QRCodeStyling } from "./core/QRCodeStyling";
 import * as browserUtils from "./tools/browserUtils";
-import * as types from "./types";
 
-export * from "./tools/browserUtils";
 export * from "./types";
 export { ErrorCorrectionPercents, QRCodeStyling };
 
 export default {
   QRCodeStyling,
-  browserUtils,
-  types
+  browserUtils
 };
