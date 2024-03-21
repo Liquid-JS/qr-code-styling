d114afe99022790262bcd5c90ddd266571a139e4
HitkoDev <development@hitko.si>
2024-03-21 19:33:41 +0100

    fix: image size

--- src/tools/calculateImageSize.ts
+++ src/tools/calculateImageSize.ts
@@ -25,7 +25,13 @@ export function calculateImageSize({
   const hideDots = { x: 0, y: 0 };
   const imageSize = { x: 0, y: 0 };
 
-  if (originalHeight <= 0 || originalWidth <= 0 || maxHiddenDots <= 0 || dotSize <= 0) {
+  if (
+    originalHeight <= 0 ||
+    originalWidth <= 0 ||
+    maxHiddenDots <= 0 ||
+    dotSize <= 0 ||
+    4 * margin ** 2 > maxHiddenDots
+  ) {
     return {
       height: 0,
       width: 0,
@@ -34,62 +40,46 @@ export function calculateImageSize({
     };
   }
 
-  if (maxHiddenAxisDots) {
-    // Adjust max size based on image aspect ratio and margin
-    const maxD = maxHiddenAxisDots - 2 * margin;
-    if (originalWidth > originalHeight) {
-      const newH = (maxD * originalHeight) / originalWidth + 2 * margin;
-      maxHiddenDots = Math.min(maxHiddenDots, maxHiddenAxisDots * newH);
-    } else {
-      const newW = (maxD * originalWidth) / originalHeight + 2 * margin;
-      maxHiddenDots = Math.min(maxHiddenDots, maxHiddenAxisDots * newW);
-    }
+  //If set, ensure maxHiddenAxisDots is odd and at least 1
+  if (maxHiddenAxisDots && maxHiddenAxisDots % 2 == 0) maxHiddenAxisDots = Math.max(1, maxHiddenAxisDots - 1);
+
+  //Determine the bigger and smaller image dimension
+  const dimMax = Math.max(originalWidth, originalHeight);
+  const dimMin = Math.min(originalWidth, originalHeight);
+
+  //Find max image size along the bigger dimension
+  let lower = 0,
+    upper = (maxHiddenAxisDots || maxHiddenDots) - 2 * margin;
+  do {
+    const size = lower + (upper - lower) / 2;
+    let dotsMax = Math.max(1, Math.ceil(size + 2 * margin));
+    if (dotsMax % 2 == 0) dotsMax++;
+    let dotsMin = Math.max(1, Math.ceil((size * dimMin) / dimMax + 2 * margin));
+    if (dotsMin % 2 == 0) dotsMin++;
+
+    if (dotsMax * dotsMin > maxHiddenDots) upper = size;
+    else lower = size;
+  } while (Math.abs(lower - upper) > 0.001);
+
+  //Calculate how many dots the given size corresponds to
+  const size = lower;
+  let hideMax = Math.max(1, Math.ceil(size + 2 * margin));
+  if (hideMax % 2 == 0) hideMax++;
+  let hideMin = Math.max(1, Math.ceil((size * dimMin) / dimMax + 2 * margin));
+  if (hideMin % 2 == 0) hideMin++;
+
+  //Set hidden dots along bigger and smaller dimension to the right axis
+  if (originalWidth > originalHeight) {
+    hideDots.x = hideMax;
+    hideDots.y = hideMin;
+  } else {
+    hideDots.x = hideMin;
+    hideDots.y = hideMax;
   }
 
-  const m2 = margin ** 2;
-  const w2 = originalWidth ** 2;
-  const h2 = originalHeight ** 2;
-
-  //Margin-adjuseted scale
-  const scale =
-    (Math.sqrt(w2 * m2 + h2 * m2 + originalWidth * originalHeight * (maxHiddenDots - 2 * m2)) -
-      originalHeight * margin -
-      originalWidth * margin) /
-    (originalHeight * originalWidth);
-
-  hideDots.x = Math.max(1, Math.floor(originalWidth * scale + 2 * margin));
-  hideDots.y = Math.max(1, Math.floor(originalHeight * scale + 2 * margin));
-
-  if (hideDots.x > hideDots.y) {
-    //The count of dots should be odd
-    if (hideDots.x % 2 === 0) hideDots.x--;
-
-    hideDots.y = Math.max(1, Math.ceil(((hideDots.x - 2 * margin) * originalHeight) / originalWidth + 2 * margin));
-
-    //The count of dots should be odd
-    if (hideDots.y % 2 === 0) hideDots.y--;
-  } else if (hideDots.x < hideDots.y) {
-    //The count of dots should be odd
-    if (hideDots.y % 2 === 0) hideDots.y--;
-
-    hideDots.x = Math.max(1, Math.ceil(((hideDots.y - 2 * margin) * originalWidth) / originalHeight + 2 * margin));
-
-    //The count of dots should be odd
-    if (hideDots.x % 2 === 0) hideDots.x--;
-  }
-
+  //Fit image to the hidden dots
   const maxW = (hideDots.x - 2 * margin) * dotSize;
   const maxH = (hideDots.y - 2 * margin) * dotSize;
-
-  if (maxW <= 0 || maxH <= 0) {
-    return {
-      height: 0,
-      width: 0,
-      hideYDots: hideDots.y,
-      hideXDots: hideDots.x
-    };
-  }
-
   imageSize.x = maxW;
   imageSize.y = (maxW * originalHeight) / originalWidth;
   if (imageSize.y > maxH) {
