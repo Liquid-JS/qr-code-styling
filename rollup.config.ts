56a64674c16b3ef577dd38413627bacdf5ce787a
Hitko Development <development@hitko.si>
2024-12-16 17:31:19 +0100

    build: update build tools

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,14 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
 import compiler from "@liquid-js/rollup-plugin-closure-compiler";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
-import { RollupOptions } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import { type RollupOptions } from "rollup";
 import { minifyTemplateLiterals } from "rollup-plugin-minify-template-literals";
 import replace from "rollup-plugin-replace-regex";
-import typescript from "rollup-plugin-typescript2";
 
 const repl = replace({
   regexValues: {
@@ -23,7 +20,11 @@ const repl = replace({
 repl.renderChunk = repl.__renderChunk;
 
 const common = () => [
-  typescript(),
+  typescript({
+    declaration: true,
+    declarationDir: "./lib/",
+    inlineSources: true
+  }),
   commonjs(),
   minifyTemplateLiterals({
     options: {
@@ -37,11 +38,11 @@ const bundle = () => [
   compiler({
     language_in: "ECMASCRIPT_2020",
     language_out: "ECMASCRIPT_2020"
-  }) as any,
+  }),
   terser()
 ];
 
-const config: RollupOptions[] = [
+export default new Array<RollupOptions>(
   {
     input: "src/index.ts",
     output: {
@@ -76,11 +77,9 @@ const config: RollupOptions[] = [
         language_out: "ECMASCRIPT_2020",
         formatting: "PRETTY_PRINT",
         debug: "true"
-      }) as any,
+      }),
       repl,
       terser({ mangle: false, ecma: 2020, output: { beautify: true } })
     ]
   }
-];
-
-export default config;
+);
